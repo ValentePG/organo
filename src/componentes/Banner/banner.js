925474ec91d5666b07fd6f2be8187f7ab6420ebe
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2023-09-05 11:37:34 -0300

    primeira aula

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+export const Banner = () => {
+  // JSX
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+    </header>
+  )
+}
+
