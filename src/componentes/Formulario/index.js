d19bbe9d7c42e189467312946f9c64c37ad2fb4c
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2023-09-06 13:54:40 -0300

    Finalizado

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,15 +6,7 @@ import { useState } from 'react'
 
 const Formulario = (props) => {
 
-  const times = [
-    'Programação',
-    'Front-End',
-    'Data Science',
-    'Devops',
-    'UX e Design',
-    'Mobile',
-    'Inovação e Gestão'
-  ]
+  
 
   const [nome, setNome] = useState('')
   const [cargo, setCargo] = useState('')
@@ -29,6 +21,10 @@ const Formulario = (props) => {
       imagem,
       time
     })
+    setNome('')
+    setCargo('')
+    setImagem('')
+    setTime('')
   }
 
   return (
@@ -61,7 +57,7 @@ const Formulario = (props) => {
           <ListaSuspensa 
             obrigatorio={true}
             label="Time"
-            itens={times}
+            itens={props.times}
             valor={time}
             aoAlterado={valor => setTime(valor)}
           />
